20ab3562deb3533cb0de97f6600e2dc4c896d0a9
ramonNobrega <rb0nobrega@gmail.com>
2015-04-21 20:25:56 -0300

    +Logo na tela inicial/*Personalização de tema

--- app/src/main/java/com/example/snakechild/ciadobusao/MainActivity.java
+++ app/src/main/java/com/example/snakechild/ciadobusao/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +13,9 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Hide the Status Bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 
 
