c1164ef87c3708219a35d68e156778ca45d795bc
ramonNobrega <rb0nobrega@gmail.com>
2015-05-06 11:50:50 -0300

    Fix button Cancel/Criar

--- app/src/main/java/com/example/snakechild/ciadobusao/NovoEncontroActivity.java
+++ app/src/main/java/com/example/snakechild/ciadobusao/NovoEncontroActivity.java
@@ -1,8 +1,10 @@
 package com.example.snakechild.ciadobusao;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.TextView;
 
@@ -46,5 +48,10 @@ public class NovoEncontroActivity extends Activity {
     @Override
     public void onBackPressed() {
         super.onBackPressed();
+    }
+
+    public void onBack(View v){
+        onBackPressed();
+        finish();
     }
 }
