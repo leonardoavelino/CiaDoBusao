a88806f3fcc2f533b7e010e1bd7e1d41b899ea3d
CelioBarros <celio.barros.filho@gmail.com>
2015-05-27 01:07:54 -0300

    Não está aparecendo corretamente o nome dos confirmados

--- app/src/main/java/com/example/snakechild/ciadobusao/DetalhesEncontroActivity.java
+++ app/src/main/java/com/example/snakechild/ciadobusao/DetalhesEncontroActivity.java
@@ -1,11 +1,15 @@
 package com.example.snakechild.ciadobusao;
 
+import android.content.Intent;
 import android.content.res.TypedArray;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -26,8 +30,10 @@ public class DetalhesEncontroActivity extends BaseActivity {
     private TypedArray navMenuIcons;
     private static String idEncontro = "";
     private static String nomeDono = "";
+    private static String participantesEncontro = "";
     private TextView nomeDoEncontro, donoDoEncontro, linhaDoEncontro, refDoEncontro, dataDoEncontro, horaDoEncontro;
-    private TextView participante;
+    private TextView participantes;
+    private Button confirmaPresencaButton, saindoButton;
 
     public static void setEncontro(String encontro) {
         DetalhesEncontroActivity.idEncontro = encontro;
@@ -46,6 +52,7 @@ public class DetalhesEncontroActivity extends BaseActivity {
                     dataDoEncontro.setText((String) parseObjects.get(0).get("data"));
                     horaDoEncontro.setText((String) parseObjects.get(0).get("horario"));
                     donoDoEncontro.setText(getDonoDoEncontro((String) parseObjects.get(0).get("idDono")));
+
                 }
             }
         });
@@ -82,9 +89,75 @@ public class DetalhesEncontroActivity extends BaseActivity {
         refDoEncontro = (TextView) findViewById(R.id.idDetalheRefEncontro);
         dataDoEncontro = (TextView) findViewById(R.id.idDetalheDataEncontro);
         horaDoEncontro = (TextView) findViewById(R.id.idDetalheHoraEncontro);
+        participantes = (TextView) findViewById(R.id.idConfirmaram);
+
+        confirmaPresencaButton = (Button) findViewById(R.id.idConfirmaButton);
+        saindoButton = (Button) findViewById(R.id.idSaindoButton);
 
         customizeItems();
         getDetalhesEncontro();
+        setButtonsVisible();
+        getPerfisConfirmaram();
+    }
+
+    private void setButtonsVisible(){
+        ParseQuery query = new ParseQuery("PerfisConfirmaram");
+        query.whereEqualTo("idUsuario", PerfilActivity.idUsuario);
+        query.whereEqualTo("idEncontro", idEncontro);
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> parseObjects, ParseException e) {
+                if (parseObjects != null) {
+                    if (!parseObjects.isEmpty()){
+                        confirmaPresencaButton.setVisibility(View.INVISIBLE);
+                        confirmaPresencaButton.setClickable(false);
+                        saindoButton.setVisibility(View.VISIBLE);
+                        saindoButton.setClickable(true);
+                    }
+                }
+            }
+        });
+    }
+
+    private void getPerfisConfirmaram(){
+        ParseQuery query = new ParseQuery("PerfisConfirmaram");
+        query.whereEqualTo("idEncontro", idEncontro);
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> parseObjects, ParseException e) {
+                if (parseObjects != null) {
+                    for (int i = 0; i < parseObjects.size(); i++) {
+                        ParseQuery query = new ParseQuery("Usuario");
+                        query.whereEqualTo("id_user", (String) parseObjects.get(i).get("idUsuario"));
+                        query.findInBackground(new FindCallback<ParseObject>() {
+                            @Override
+                            public void done(List<ParseObject> parseObjects, ParseException e) {
+                                if (parseObjects != null) {
+                                    participantesEncontro = participantesEncontro + (String) parseObjects.get(0).get("nome") + ",";
+                                }
+                            }
+                        });
+                    }
+                }
+            }
+        });
+        participantes.setText(participantesEncontro);
+        participantesEncontro="";
+    }
+
+    public void confirmaPresenca(View v) {
+        final ParseObject usuario = new ParseObject("PerfisConfirmaram");
+        usuario.put("idUsuario", PerfilActivity.idUsuario);
+        usuario.put("idEncontro", idEncontro);
+        usuario.saveInBackground();
+
+        if (Build.VERSION.SDK_INT >= 11) {
+            recreate();
+        } else {
+            Intent intent = getIntent();
+            finish();
+            startActivity(intent);
+        }
     }
 
     public void customizeItems() {
